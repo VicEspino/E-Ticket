4107aa4027aedd9b4ae56082495d50012ea2762b
VicEspino <taxgype1@gmail.com>
2019-11-04 01:03:46 -0600

    Diseño principal echo.

---
+++ src/controllers/Principal.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import com.jfoenix.controls.JFXTreeTableView;
+import javafx.fxml.FXML;
+
+public class Principal {
+
+    @FXML
+    private JFXTreeTableView<?> table_tickets;
+
+    @FXML
+    private JFXTreeTableView<?> table_resumen_ticket;
+
+}
